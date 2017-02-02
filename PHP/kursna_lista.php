80a2ec59f682faae94bc46089c399e9f5b4368bb
AionCode <astojanovic84@gmail.com>
2017-02-02 17:22:30 +0100

    Added more stuff

---
+++ PHP/kursna_lista.php
@@ -0,0 +1,106 @@
+<?php
+  $kl = array (
+    "RSD" => array(
+      "RSD" => 1,
+      "EUR" => 0.00807,
+      "USD" => 0.00864,
+    ),
+    "EUR" => array(
+      "RSD" => 123.960,
+      "EUR" => 1,
+      "USD" => 1.07060,    
+    ),
+    "USD" => array(
+      "RSD" => 115.785,
+      "EUR" => 0.93406,
+      "USD" => 1,
+    ),
+  );
+
+  $r="";
+  if(isset($_POST['submit'])){
+    if ($_POST['v1']=='-' OR $_POST['v2']=='-' OR $_POST['iznos']==""){
+      $r= "Sve mora da se unese!";
+    } else {
+      $v1=$_POST['v1'];
+      $v2=$_POST['v2'];
+      $i = $_POST['iznos'];
+      $r = number_format($_POST['iznos'],2,",",".")." ".$v1." = ".number_format($kl[$v1][$v2]*$i,2,",",".")." ".$v2;
+    }
+  }
+   ?>
+<html>
+	<head>
+		<title>Kursna lista</title>
+	</head>
+  <style>
+    body {
+      font-family:Tahoma;
+    }
+    h1 {
+      text-align:center;
+      font-size:30px;
+    }
+    label {
+      display: inline-block;
+      width:80px;
+      margin:5px 5px 5px 50px;
+    }
+    .rez {
+      border:3px solid #555;
+      border-radius:10px;
+      background:#ddd;
+      margin:0px 30px 20px 30px;
+      height:40px;
+      text-align:center;
+      font-size:20px;
+      padding-top:10px;
+    }
+    .s {
+      margin:30px 5px 5px 50px;
+    }
+    .k {
+      border:3px solid #555;
+      border-radius:10px;
+      width:400px;
+      margin:30px;
+      background:#F5FF85;
+    }
+  </style>
+	<body>
+  <div class="k">  
+    <h1>Konvertovanje valute</h1>
+    
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+      
+      <p class="rez"><?php echo $r; ?></p>
+      
+      <label>Iznos:</label>
+      <input type="number" name="iznos"><br>
+      
+      <label>Iz valute:</label>
+      <select name="v1">
+        <option value='-'>-</option>
+        <?php
+        foreach($kl as $k => $v){
+          echo "<option value='$k'>$k</option>";
+        }
+        ?>
+      </select><br>
+      
+      <label>U valutu:</label>
+      <select name="v2">
+        <option value='-'>-</option>
+        <?php
+        foreach($kl as $k => $v){
+          echo "<option value='$k'>$k</option>";
+        }
+        ?>
+      </select><br>
+      
+      <input type="submit" name="submit" value="Konvertuj" class="s">
+      
+    </form>
+  </div>
+	</body>
+</html>
